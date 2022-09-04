67c924a21f3f4d84b8afb81829d981c3d82d36ab
J██████ <dev.nullinside@gmail.com>
2022-09-04 14:53:51 -0400

    Updating package versions

--- go.bat
+++ go.bat
@@ -11,7 +11,7 @@ if [%version%]==[] (
 echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 dotnet build streaming-tools/streaming-tools.sln /p:Configuration=Release /p:Platform="Any CPU"
-xcopy streaming-tools\streaming-tools\bin\Release\net5.0 "v%version%\streaming-tools\" /s /e
-xcopy streaming-tools\WindowsKeyboardHook\bin\Release\net5.0 "v%version%\WindowsKeyboardHook\" /s /e
+xcopy streaming-tools\streaming-tools\bin\Release\net6.0 "v%version%\streaming-tools\" /s /e
+xcopy streaming-tools\WindowsKeyboardHook\bin\Release\net6.0 "v%version%\WindowsKeyboardHook\" /s /e
 
 "C:\Program Files\7-Zip\7z.exe" a "v%version%.zip" "v%version%"
